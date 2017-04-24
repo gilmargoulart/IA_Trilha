4cc4a7f4b448271ea26f79181b4830ec78c29519
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2017-04-24 20:32:06 -0300

    Adicionado tabuleiro e peças

--- src/br/com/trilha/mainProgram/MainProgram.java
+++ src/br/com/trilha/mainProgram/MainProgram.java
@@ -1,11 +1,49 @@
 package br.com.trilha.mainProgram;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
 public class MainProgram {
 
-	public static void main(String[] args) {
-		
-		
+	private JFrame frame;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainProgram window = new MainProgram();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the application.
+	 */
+	public MainProgram() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1207, 807);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(MainProgram.class.getResource("/br/com/trilha/imagens/tabuleiro.png")));
+		lblNewLabel.setBounds(10, 11, 1126, 595);
+		frame.getContentPane().add(lblNewLabel);
+	}
 }
